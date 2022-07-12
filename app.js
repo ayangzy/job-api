6659e2519d3885c0a8407dac28bbe9bb2f768cab
Ayange Felix <ayangefelix8@gmail.com>
2022-07-12 12:07:21 +0100

    add middleware

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const express = require('express');
-
 const app = express();
+const bodyParser = require('body-parser');
+
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
 
 
+app.use(bodyParser.json());
 
+app.get("/job-api", (req, res, next) => {
+    res.send("<h1>Hello welcome to our job api board</h1>");
+});
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
