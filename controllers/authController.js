ca667c17a9a7654fa7fd68ff0a51c509729ef01b
Ayange Felix <ayangefelix8@gmail.com>
2022-07-13 21:17:12 +0100

    implement login functionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,17 +1,37 @@
 const User = require('../models/userModel');
 const { StatusCodes } = require('http-status-codes');
-const { BadRequestError } = require('../errors');
+const { BadRequestError, UnauthenticatedError } = require('../errors');
 
 exports.register = async(req, res) => {
     const user = await User.create({ ...req.body });
     const token = user.createJWT();
-    
-    res.status(StatusCodes.CREATED).send({status: 'success', data: {
+
+    res.status(StatusCodes.CREATED).send({status: 'success', message: 'User successfully SignedUp', data: {
     user: { name: user.name, email: user.email }, 
     accessToken: token } })
 }
 
 
 exports.login = async(req, res, next) => {
-    res.send("login..");
+
+    const { email, password } = req.body;
+    if(!email || !password){
+        throw new BadRequestError('Please enter your email and password');
+    }
+
+    const user = await User.findOne({ email });
+    if(!user){
+        throw new UnauthenticatedError('Invalid Credentials')
+    }
+
+    const isPasswordCorrect = await user.comparePassword(password);
+    if(!isPasswordCorrect){
+        throw new UnauthenticatedError('Invalid Credentials')
+    }
+
+    const token = user.createJWT();
+    res.status(StatusCodes.OK).send({status: 'success', message: 'User successfully SignedIn', data: {
+    user: { name: user.name, email: user.email }, 
+    accessToken: token } })
+
 }
--- models/userModel.js
+++ models/userModel.js
@@ -41,6 +41,11 @@ UserSchema.methods.createJWT = function(){
     return token;
 }
 
+UserSchema.methods.comparePassword = async function(candidatePassword){
+    const isMatch = await bcrypt.compare(candidatePassword, this.password);
+    return isMatch;
+}
+
 module.exports = mongoose.model('User', UserSchema);
 
 
