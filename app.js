196b1d81728d8b892eb4ab36bc69e3f019a5fe64
felix ayange <felixdecoder2020@gmail.com>
2023-04-21 00:08:46 +0100

    dockerized app

--- app.js
+++ app.js
@@ -1,61 +1,61 @@
-require('dotenv').config();
-require('express-async-errors');
+require('dotenv').config()
+require('express-async-errors')
+const cors = require('cors')
+const express = require('express')
+const app = express()
+const fileUpload = require('express-fileupload')
 
-const express = require('express');
-const app = express();
-const fileUpload = require('express-fileupload');
-
-const dbConnect = require('./db/dbConnect');
-const bodyParser = require('body-parser');
+const dbConnect = require('./db/dbConnect')
+const bodyParser = require('body-parser')
 // USE V2
-const cloudinary = require('cloudinary').v2;
+const cloudinary = require('cloudinary').v2
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.CLOUD_API_KEY,
   api_secret: process.env.CLOUD_API_SECRET,
-});
+})
 
-const authenticateUser = require('./middleware/authentication');
+const authenticateUser = require('./middleware/authentication')
 
 //routers
-const authRoutes = require('./routes/authRoute');
-const jobRoutes = require('./routes/jobRoute');
-const uploadRoutes = require('./routes/uploadRoute');
-const sendEmailRoute = require('./routes/sendEmailRoute');
-const paystackRoutes = require('./routes/paystackRoute');
+const authRoutes = require('./routes/authRoute')
+const jobRoutes = require('./routes/jobRoute')
+const uploadRoutes = require('./routes/uploadRoute')
+const sendEmailRoute = require('./routes/sendEmailRoute')
+const paystackRoutes = require('./routes/paystackRoute')
 //error handlers
-const notFoundMiddleware = require('./middleware/not-found');
-const errorHandlerMiddleware = require('./middleware/error-handler');
+const notFoundMiddleware = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
 
+app.use(bodyParser.json())
+app.use(fileUpload({ useTempFiles: true }))
 
-
-app.use(bodyParser.json());
-app.use(fileUpload({ useTempFiles: true }));
-
+app.get('/', (req, res) => {
+  res.status(200).send({ message: 'Welcome to job api' })
+})
 //endpoints
-app.use('/api/v1/auth', authRoutes);
-app.use('/api/v1/jobs', authenticateUser, jobRoutes);
-app.use('/api/v1/uploads', uploadRoutes);
-app.use('/api/v1/emails', sendEmailRoute);
-app.use('/api/v1/payments', paystackRoutes);
+app.use('/api/v1/auth', authRoutes)
+app.use('/api/v1/jobs', authenticateUser, jobRoutes)
+app.use('/api/v1/uploads', uploadRoutes)
+app.use('/api/v1/emails', sendEmailRoute)
+app.use('/api/v1/payments', paystackRoutes)
 
-app.use(notFoundMiddleware);
-app.use(errorHandlerMiddleware);
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
+app.enable('trust proxy')
+app.use(cors({}))
+const PORT = process.env.PORT || 3000
 
-
-const PORT = process.env.PORT || 3000;
-
-const start = async() => {
-   try {
-    await dbConnect(process.env.MONGO_URI);
+const start = async () => {
+  try {
+    await dbConnect()
 
     app.listen(PORT, () => {
-        console.log(`Server is listening on port ${PORT} ...`);
-    });
-
-   } catch (error) {
-       console.log(error)
-   }
+      console.log(`Server is listening on port ${PORT} ...`)
+    })
+  } catch (error) {
+    console.log(error)
+  }
 }
 
-start();
+start()
--- db/dbConnect.js
+++ db/dbConnect.js
@@ -1,14 +1,29 @@
 const mongoose = require('mongoose')
+const {
+  MONGO_USER,
+  MONGO_PORT,
+  MONGO_PASSWORD,
+  MONGO_IP,
+} = require('../config/config')
+mongoose.set('strictQuery', false)
 
-const connectDB = (url) => {
-  return mongoose.connect(url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }).then(() =>{
-      console.log("db connection succesful")
-  }).catch((error) => {
-    console.log(error)
-  });
+const url = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`
+
+//const url = `mongodb://localhost:27017/job`
+
+const connectDB = () => {
+  return mongoose
+    .connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log('db connection succesful')
+    })
+    .catch((error) => {
+      console.log(error)
+      setTimeout(connectDB, 2000)
+    })
 }
 
 module.exports = connectDB
